a053e260a1633bf1ba3893e23487e548728be0a4
Armond Snowden <armond.z.snowden@gmail.com>
2019-07-12 11:16:15 -0400

    justify-content flex start toggle added

---
+++ script/index.js
@@ -0,0 +1,8 @@
+//js
+
+const flexStart = document.querySelector('[ data-flex-start ]');
+const flexStartDiv = document.querySelector('[ data-flex-start-div ]');
+flexStart.addEventListener('click', () => {
+    flexStartDiv.classList.toggle('hidden');
+console.log('sadfkedf')
+});
